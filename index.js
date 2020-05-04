dc5129ef2ea389ca359032df8586b9bde0dca3af
Refinn Digital <shedrackjennifer@gmail.com>
2020-05-04 03:17:04 -0700

    deleted todo

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('this is my index'); 
+'use strict'
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use('/assets', express.static(path.resolve(__dirname, './assets')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My Todo Application - Changed'
+    });
+});
+
+app.get('/login', (reg, res) => {
+    res.send('this is our login page');
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
+
