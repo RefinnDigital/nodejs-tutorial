60e637ffb1c2e074563f2d02c2f2570b7702ecdb
Refinn Digital <shedrackjennifer@gmail.com>
2020-05-20 04:53:35 -0700

    Added app config and libs

---
+++ Libs/validators.js
@@ -0,0 +1,25 @@
+const { body, query, param } = require('express-validator');
+
+
+const VALIDATORS = {
+    TODO: {
+        CreateTodo: [
+            body('name').exists().withMessage('Name is required')
+                .isString().withMessage('Name must be a string')
+        ],
+        GetTodo: [
+            query('id').exists().withMessage('id is required').isMongoId().withMessage('Id passed is invalid')
+        ],
+        UpdateTodo: [
+            param('id').exists().withMessage('id is required').isMongoId().withMessage('Id passed is invalid'),
+            body('name').exists().withMessage('Name is required')
+                .isString().withMessage('Name must be a string')
+        ],
+        DeleteTodo: [
+            param('id').exists().withMessage('id is required').isMongoId().withMessage('Id passed is invalid'),
+
+        ]
+    }
+}
+
+module.exports = VALIDATORS;
